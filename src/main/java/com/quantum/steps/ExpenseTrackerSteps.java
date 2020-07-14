65e32618466c0a663ebc8ebd7614ea683c010b8c
Dushyantan Satike <dushyantans@perfectomobile.com>
2020-07-14 18:38:49 +0530

    Added Simulator native test

--- src/main/java/com/quantum/steps/ExpenseTrackerSteps.java
+++ src/main/java/com/quantum/steps/ExpenseTrackerSteps.java
@@ -1,7 +1,11 @@
 package com.quantum.steps;
 
+import org.openqa.selenium.By;
+
 import com.qmetry.qaf.automation.step.QAFTestStepProvider;
+import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebElement;
 import com.quantum.pages.ExpenseTrackerLoginPage;
+import com.quantum.utils.ReportUtils;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -22,5 +26,15 @@ public class ExpenseTrackerSteps {
 	@When("I enter \"(.*?)\" and \"(.*?)\" in native login screen")
 	public void iEnterLoginDetilsInNativeLoginScreen(String email, String password) {
 		new ExpenseTrackerLoginPage().loginNative(email, password);
+	}
+	
+	@Then("I verify Hello text")
+	public void iverifyHelloText() {
+		ReportUtils.logAssert("Verify Hello app.", new QAFExtendedWebElement(By.xpath("//*[@name='Hello World']")).isDisplayed());
+	}
+	
+	@When("I click Hello world button")
+	public void iclickHelloWorldButton() {
+		 new QAFExtendedWebElement(By.xpath("//*[@name='Hello World']")).click();
 	}
 }
